9bbac8d3fb6d61f4e8cbcfa7ba8ce55d69b42f8e
wayneleehan <islumberjacker@gmail.com>
2024-12-13 17:54:09 +0800

    Create index.js

---
+++ Drum Kit Starting Files-2/index.js
@@ -0,0 +1,64 @@
+
+function playSound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/crash.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/kick-bass.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/snare.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-1.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/tom-2.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("sounds/tom-3.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio("sounds/tom-4.mp3");
+            kick.play();
+            break;
+        default:
+            console.log(key);
+    }
+}
+
+// Function to add animation to the button
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    if (activeButton) {
+        activeButton.classList.add("pressed");
+
+        setTimeout(function () {
+            activeButton.classList.remove("pressed");
+        }, 100);
+    }
+}
+
+// Add event listeners to all drum buttons
+var drumButtons = document.querySelectorAll(".drum");
+
+drumButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        var buttonInnerHTML = this.innerHTML;
+        playSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+});
+
+// Add event listener for key presses
+document.addEventListener("keydown", function (event) {
+    playSound(event.key);
+    buttonAnimation(event.key);
+});
